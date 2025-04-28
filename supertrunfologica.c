26d728eb9a0167a7c161534d68a2ba8b515aa9db
brrs766 <br1597431@gmail.com>
2025-04-28 15:46:50 +0000

    Novo commit

--- supertrunfologica.c
+++ supertrunfologica.c
@@ -2,5 +2,16 @@
 
 int main (){
     printf("=== Desafio: Super Trunfo - Países ===\n \n");
-    
+
+        // Variáveis da primeira carta
+        char estado1;
+        char codigo1[4];
+        char cidade1[50];
+        int populacao1;
+        int turisticos1;
+        float area1;
+        float pib1;
+        float densidade1, pibpercapita1;
+
+        
 }
